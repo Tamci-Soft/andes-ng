578cf75ff5c002d949eacd4a74574810551081f0
QA Judge <qa@local>
2026-09-15 11:15:30 -0500

    fix: deduplicate listbox exports from union-merge artifact

--- packages/primitives/src/index.ts
+++ packages/primitives/src/index.ts
@@ -1,17 +1,4 @@
 export { AndesButtonPrimitive } from './lib/button-primitive';
-export {
-  ANDES_LIST_TYPEAHEAD_DEBOUNCE,
-  AndesListNavigation,
-} from './lib/listbox/list-navigation';
-export { AndesListNavigationItem } from './lib/listbox/list-navigation-item';
-export { AndesListNavigationKeys } from './lib/listbox/list-navigation-keys';
-export type {
-  AndesListFocusMode,
-  AndesListNavigationConfig,
-  AndesListNavigationItemRef,
-  AndesListOrientation,
-  AndesListTextDirection,
-} from './lib/listbox/list-navigation-types';
 export * from './lib/overlay';
 export {
   ANDES_LIST_TYPEAHEAD_DEBOUNCE,
